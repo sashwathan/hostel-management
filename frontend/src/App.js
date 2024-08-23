6fdfc8dded2b61e854a387a8e0b75d65130a4583
sashwathan <ssashwathan@gmail.com>
2024-08-23 18:13:08 +0530

    navbar and menu

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,12 +2,27 @@
 import Homepage from './components/Homepage';
 import './App.css';
 import Secondpage from './components/Secondpage';
+import Navbar from './components/Navbar';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+
 
 function App() {
+  const routes =(
+    <Router>
+      <Routes>
+        <Route path='/' exact element={<Homepage />}/>
+        <Route path='/secondpage' exact element={<Secondpage />}/>
+      </Routes>
+       </Router>
+  );
   return (
     <div className="App">
+      <div>
+      {routes}
+    </div>
+      {/* <Navbar/>
      <Homepage />
-     <Secondpage/>
+     <Secondpage/> */}
     </div>
   );
 }
