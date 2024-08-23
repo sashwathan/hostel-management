119c8cd2c963f79b371f707e853140c56b095e65
sashwathan <ssashwathan@gmail.com>
2024-08-23 15:45:22 +0530

    changed message

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> hello world from sashwathan
+          Edit <code>src/App.js</code> hello hari
         </p>
         <a
           className="App-link"
