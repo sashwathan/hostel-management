ddb0f83bc5b5a0f1e0c5f352a533c1975b85356a
sashwathan <ssashwathan@gmail.com>
2024-08-23 16:00:34 +0530

    added second page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,13 @@
 
 import Homepage from './components/Homepage';
 import './App.css';
+import Secondpage from './components/Secondpage';
 
 function App() {
   return (
     <div className="App">
      <Homepage />
+     <Secondpage/>
     </div>
   );
 }
